f96bc177da4ea98dde43f5a227f11fa4b5d088dd
Mark Wainwright <wainwrightml@gmail.com>
2024-04-18 17:52:53 +0100

    removed some stuff

--- install_archives.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Clean any previous Dokka docs.
-rm -rf docs/api
-
-# Build the new Dokka docs.
-./gradlew clean installArchives --no-daemon --no-parallel
--- upload_archives.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Deploy updated docs
-sh deploy-docs.sh
-
-# Upload release to maven
-./gradlew uploadArchives -PSONATYPE_NEXUS_USERNAME=$SONATYPE_NEXUS_USERNAME -PSONATYPE_NEXUS_PASSWORD=$SONATYPE_NEXUS_PASSWORD --no-daemon --no-parallel
